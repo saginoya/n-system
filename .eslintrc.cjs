0183cde84f8fa22320593597efe9e45a722104e9
saginoya <saginoya@create-nippo.co.jp>
2024-05-31 17:04:35 +0900

    Update Tailwindcss settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
+    'plugin:tailwindcss/recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  plugins: ['tailwindcss']
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)'
+      }
+    }
   },
   plugins: []
 }
