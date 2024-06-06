04b73663becfe2c26e3737fe480c17ba2bab06be
saginoya <saginoya@create-nippo.co.jp>
2024-06-06 13:14:01 +0900

    Add NApp

---
+++ src/components/NApp.ce.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  jsonPath: string
+  topPage?: boolean
+}>()
+</script>
+
+<template>
+  <Transition name="fade">
+    <n-layout-default :json-path="jsonPath" :top-page="topPage">
+      <slot />
+    </n-layout-default>
+  </Transition>
+</template>
