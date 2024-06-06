4e0c24ce1b0842abafebfa5cbac995340e01543b
saginoya <saginoya@create-nippo.co.jp>
2024-06-06 13:13:43 +0900

    Update NMain

--- src/components/NMain.ce.vue
+++ src/components/NMain.ce.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main class="px-4 py-2">
-    <div class="flex w-full max-w-screen-lg flex-col gap-2">
+    <div class="flex w-full max-w-screen-lg flex-col gap-4">
       <slot />
     </div>
   </main>
