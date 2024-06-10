959e4b8239f8edb00b1f874a667c7d37f5ef34f3
saginoya <saginoya@create-nippo.co.jp>
2024-06-10 13:40:32 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/css/main.css'
 
 // Components
 import NApp from './components/NApp.ce.vue'
+import NBanner from './components/NBanner.ce.vue'
 import NCard from './components/NCard.ce.vue'
 import NChip from './components/NChip.ce.vue'
 import NContainer1col from './components/NContainer1col.ce.vue'
@@ -14,13 +15,12 @@ import NHeader from './components/NHeader.ce.vue'
 import NHeaderTool from './components/NHeaderTool.ce.vue'
 import NImage from './components/NImage.ce.vue'
 import NLayoutDefault from './components/NLayoutDefault.ce.vue'
-import NLinkDoc from './components/NLinkDoc.ce.vue'
-import NLinkExternal from './components/NLinkExternal.ce.vue'
-import NLinkInternal from './components/NLinkInternal.ce.vue'
+import NLink from './components/NLink.ce.vue'
 import NListNav from './components/NListNav.ce.vue'
 import NListNavRow from './components/NListNavRow.ce.vue'
 import NMain from './components/NMain.ce.vue'
 import NMainVisual from './components/NMainVisual.ce.vue'
+import NSheetCentral from './components/NSheetCentral.ce.vue'
 import NSignature from './components/NSignature.ce.vue'
 import NTitleLv1Site from './components/NTitleLv1Site.ce.vue'
 import NTitleLv2Page from './components/NTitleLv2Page.ce.vue'
@@ -29,6 +29,7 @@ import NTitleLv4 from './components/NTitleLv4.ce.vue'
 import NTitleLv5 from './components/NTitleLv5.ce.vue'
 
 customElements.define('n-app', defineCustomElement(NApp))
+customElements.define('n-banner', defineCustomElement(NBanner))
 customElements.define('n-card', defineCustomElement(NCard))
 customElements.define('n-chip', defineCustomElement(NChip))
 customElements.define('n-container-1col', defineCustomElement(NContainer1col))
@@ -40,13 +41,12 @@ customElements.define('n-header', defineCustomElement(NHeader))
 customElements.define('n-header-tool', defineCustomElement(NHeaderTool))
 customElements.define('n-image', defineCustomElement(NImage))
 customElements.define('n-layout-default', defineCustomElement(NLayoutDefault))
-customElements.define('n-link-doc', defineCustomElement(NLinkDoc))
-customElements.define('n-link-external', defineCustomElement(NLinkExternal))
-customElements.define('n-link-internal', defineCustomElement(NLinkInternal))
+customElements.define('n-link', defineCustomElement(NLink))
 customElements.define('n-list-nav', defineCustomElement(NListNav))
 customElements.define('n-list-nav-row', defineCustomElement(NListNavRow))
 customElements.define('n-main', defineCustomElement(NMain))
 customElements.define('n-main-visual', defineCustomElement(NMainVisual))
+customElements.define('n-sheet-central', defineCustomElement(NSheetCentral))
 customElements.define('n-signature', defineCustomElement(NSignature))
 customElements.define('n-title-lv1-site', defineCustomElement(NTitleLv1Site))
 customElements.define('n-title-lv2-page', defineCustomElement(NTitleLv2Page))
