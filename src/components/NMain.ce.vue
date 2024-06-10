e460f8b31fa97305758b47de42fab38507aaff59
saginoya <saginoya@create-nippo.co.jp>
2024-06-10 16:50:17 +0900

    Update layout

--- src/components/NMain.ce.vue
+++ src/components/NMain.ce.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="px-4 py-2">
+  <main class="px-4 py-8">
     <div class="flex w-full max-w-screen-lg flex-col gap-12 py-6">
       <slot />
     </div>
---
+++ src/components/NTitleLv2Index.ce.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 class="text-center sm:text-left">
+    <slot />
+  </h2>
+</template>
+
+<style scoped>
+@import url(../assets/css/main.css);
+</style>
