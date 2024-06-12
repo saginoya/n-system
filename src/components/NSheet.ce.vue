0f0899e58906b4800fc8ee1d8b006429b3bc26c6
saginoya <saginoya@create-nippo.co.jp>
2024-06-12 13:39:41 +0900

    Update rename NSheetCentral

---
+++ src/components/NSheet.ce.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  color?: 'gray'
+}>()
+</script>
+
+<template>
+  <div
+    class="flex h-full flex-col items-center justify-center gap-4 p-4"
+    :class="{ 'bg-gray-50': color === 'gray' }"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@import url(../assets/css/main.css);
+</style>
--- src/components/NSheetCentral.ce.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex h-full items-center justify-center bg-gray-50 p-4">
-    <slot />
-  </div>
-</template>
-
-<style scoped>
-@import url(../assets/css/main.css);
-</style>
