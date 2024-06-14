c968f9af261d61d0b567bdf32ebcfa3b427820d8
saginoya <saginoya@create-nippo.co.jp>
2024-06-14 17:11:50 +0900

    Fixes NlistNavRow

--- src/components/NListNavRow.ce.vue
+++ src/components/NListNavRow.ce.vue
@@ -8,8 +8,8 @@ defineProps<{
 </script>
 
 <template>
-  <ul class="mx-auto flex max-w-5xl flex-wrap justify-around gap-2">
-    <li v-for="(link, index) in links" :key="index">
+  <ul class="mx-auto flex max-w-5xl flex-wrap justify-around justify-items-center">
+    <li v-for="(link, index) in links" :key="index" class="text-center">
       <n-link :href="link.path" :type="link.type" class="text-white">
         {{ link.name }}
       </n-link>
