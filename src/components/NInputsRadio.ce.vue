17c6c09e881fbda70b7a50a1a65610e31fa0e2fd
saginoya <saginoya@create-nippo.co.jp>
2024-06-17 11:47:11 +0900

    Update NinputsRadio

--- src/components/NInputsRadio.ce.vue
+++ src/components/NInputsRadio.ce.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import { useValidation } from '@/composables/useValidation'
+
 const props = defineProps<{
   name: string
   values: string
+  labels?: string
   title?: string
   required?: boolean
   checked?: number
 }>()
 
-const valueList = props.values.split(',')
+const valueList: string[] = props.values.split(',')
+const labelList: string[] | undefined = props.labels?.split(',')
+
+const model = ref<string>('')
+
+const { isValidation, validationMsg, validateCheck } = useValidation()
+watchEffect(() => {
+  validateCheck(model.value, props.required)
+})
 </script>
 
 <template>
   <div class="flex flex-col gap-1">
     <p v-if="title" class="font-bold">{{ title }}</p>
-    <label v-for="(value, index) in valueList" :key="index">
-      <input
-        type="radio"
-        id="radio"
-        :name="name"
-        :value="value"
-        :checked="index === checked"
-        :required="required && index === 1"
-      />
-      {{ value }}
-    </label>
-    <!--     <div class="min-h-6">
+    <div class="flex gap-1">
+      <label
+        v-for="(value, index) in valueList"
+        :key="index"
+        class="rounded px-2 py-1 text-lg hover:bg-slate-100 focus:bg-slate-100"
+      >
+        <input
+          type="radio"
+          id="radio"
+          :name="name"
+          v-model="model"
+          :value="value"
+          :checked="index === checked"
+          :required="required && index === 1"
+        />
+        {{ labelList ? labelList[index] || value : value }}
+      </label>
+    </div>
+    <div class="min-h-4">
       <n-msg-check v-if="isValidation"></n-msg-check>
       <n-msg-warning v-else>{{ validationMsg }}</n-msg-warning>
-    </div> -->
+    </div>
     <slot />
   </div>
 </template>
