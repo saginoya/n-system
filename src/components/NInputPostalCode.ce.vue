edb862109be2561c80352b9c2cd2cba0799a0cc0
saginoya <saginoya@create-nippo.co.jp>
2024-06-17 17:06:16 +0900

    Add NInputPostalCode

---
+++ src/components/NInputPostalCode.ce.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import NMsgCheck from './NMsgCheck.ce.vue'
+import NMsgWarning from './NMsgWarning.ce.vue'
+import { useValidation } from '@/composables/useValidation'
+import type { AttributesInput } from '@/types/attributesInputs'
+
+const props = withDefaults(defineProps<AttributesInput>(), {
+  name: 'postal-code',
+  autocomplete: 'postal-code',
+  inputmode: 'decimal',
+  placeholder: '101-0061',
+  pattern: '^[0-9]{3}-?[0-9]{4}$'
+})
+
+const model = ref<string>('')
+
+const { errors, isValidate, setTermsRequired, setTermsPattern, validate } = useValidation()
+setTermsRequired(props.required)
+setTermsPattern(props.pattern)
+watchEffect(() => {
+  validate(model.value)
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <label v-if="title" :for="name" class="font-bold">{{ title }}</label>
+    <input
+      type="text"
+      :id="name"
+      :name
+      :required
+      :disabled
+      :form
+      :readonly
+      :autocomplete
+      :inputmode
+      :placeholder
+      :pattern
+      class="w-full max-w-xs rounded border-2 border-gray-300 px-2 py-1 text-lg"
+      v-model="model"
+    />
+    <div class="min-h-4">
+      <n-msg-check v-if="isValidate"></n-msg-check>
+      <n-msg-warning v-else>{{ errors[0] }}</n-msg-warning>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@import url(../assets/css/main.css);
+</style>
