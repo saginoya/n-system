21fc4bd3b3d91a11fe5d376e1a5c1168ec713302
saginoya <saginoya@create-nippo.co.jp>
2024-06-26 16:47:50 +0900

    Fix NHeader

--- src/components/NHeader.ce.vue
+++ src/components/NHeader.ce.vue
@@ -14,7 +14,7 @@ defineProps<{
     <nav class="flex flex-col gap-4 overflow-y-auto">
       <n-list-nav v-if="nav.header" :links="nav.header"> </n-list-nav>
       <n-list-nav v-if="nav.footer" :links="nav.footer"> </n-list-nav>
-      <n-list-nav v-if="nav.lang" :links="nav.lang"> </n-list-nav>
+      <n-list-nav v-if="nav.related" :links="nav.related"> </n-list-nav>
     </nav>
     <slot />
   </header>
