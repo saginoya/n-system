63c3abade0ddce569344f32213e890f91c83c103
saginoya <saginoya@create-nippo.co.jp>
2024-07-03 16:02:29 +0900

    Add NElapsed

---
+++ src/components/NElapsed.ce.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <span
+      class="inline-block h-2 w-8 -translate-y-0.5 skew-x-[-45deg] border-b-2 border-l-2 border-b-slate-400 border-l-slate-400 content-['']"
+    ></span>
+    <span class="inline-block px-2">
+      <slot />
+    </span>
+    <span
+      class="inline-block h-2 w-8 -translate-y-0.5 skew-x-[45deg] border-b-2 border-r-2 border-b-slate-400 border-r-slate-400 content-['']"
+    ></span>
+  </span>
+</template>
+
+<style scoped>
+@import url(../assets/css/main.css);
+</style>
