3bc82deafb0250fddfdd91b730a8e8887f71254c
saginoya <saginoya@create-nippo.co.jp>
2024-07-04 13:36:46 +0900

    Fix NHeaderTool

--- src/components/NHeaderTool.ce.vue
+++ src/components/NHeaderTool.ce.vue
@@ -11,8 +11,7 @@ defineProps<{
     type="button"
     class="fixed bottom-2 right-2 flex size-16 flex-col items-center justify-center rounded-full border-2 border-white/0 bg-primary text-white shadow drop-shadow hover:border-white/80 focus:border-white/80"
   >
-    <IconMenu v-show="!active" class="size-8" />
-    <IconClose v-show="active" class="size-8" />
+    <component :is="active === true ? IconClose : IconMenu" class="size-8"></component>
     <span class="text-xs">MENU</span>
   </button>
 </template>
