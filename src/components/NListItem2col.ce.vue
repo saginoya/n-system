15f1cb3e536437a0647966c044bcd2a4df9a5e06
saginoya <saginoya@create-nippo.co.jp>
2024-07-08 14:11:01 +0900

    Update NListItem2col

--- src/components/NListItem2col.ce.vue
+++ src/components/NListItem2col.ce.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  header?: string
+}>()
+</script>
+
 <template>
   <li class="grid grid-cols-1 border-b p-2 sm:grid-cols-4 md:grid-cols-6 md:p-4">
     <div class="font-bold">
-      <slot name="header"></slot>
+      <slot name="header">{{ header }}</slot>
     </div>
     <div class="sm:col-span-3 md:col-span-5">
       <slot />
