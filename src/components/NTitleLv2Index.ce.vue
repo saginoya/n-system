0745fe720c151259d44202ddebf8861a7c48b610
saginoya <saginoya@create-nippo.co.jp>
2024-07-18 16:30:32 +0900

    Update NTitleLv2index

--- src/components/NTitleLv2Index.ce.vue
+++ src/components/NTitleLv2Index.ce.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  decoration: 'border' | undefined
+}>()
+
+const decorationBorder = (): boolean => {
+  return props.decoration === 'border'
+}
+</script>
+
 <template>
-  <h2 class="text-center text-2xl sm:text-left">
+  <h2
+    class="text-center text-2xl sm:text-left"
+    :class="{ 'border-l-4 border-l-current pl-4': decorationBorder() }"
+  >
     <slot />
   </h2>
 </template>
