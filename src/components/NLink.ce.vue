3bb58ae458d8b7893507e87a554c2f838b77d4aa
saginoya <saginoya@create-nippo.co.jp>
2024-07-30 14:50:46 +0900

    Fix NLink

--- src/components/NLink.ce.vue
+++ src/components/NLink.ce.vue
@@ -14,7 +14,7 @@ const { isSamePage } = useURL()
 
 <template>
   <a
-    class="inline-block rounded-full px-3 py-1 hover:bg-white/20 focus:bg-white/20"
+    class="inline-block rounded-full px-3 py-1 hover:bg-blue-200/20 focus:bg-blue-200/20"
     :class="{ 'bg-black': isSamePage(href), 'opacity-70': isSamePage(href) }"
     :href
     :target="linkTarget(type)"
