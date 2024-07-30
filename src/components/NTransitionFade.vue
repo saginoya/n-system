8623b62c2457fe4460681fc43795e7e42a788a1e
saginoya <saginoya@create-nippo.co.jp>
2024-07-30 12:53:32 +0900

    Add NTransitionFade & NTransitonScale

---
+++ src/components/NTransitionFade.vue
@@ -0,0 +1,14 @@
+<template>
+  <Transition
+    enter-active-class="transition-opacity duration-300"
+    leave-active-class="transition-opacity duration-300"
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+  >
+    <slot />
+  </Transition>
+</template>
+
+<style>
+@import url(../assets/css/main.css);
+</style>
---
+++ src/components/NTransitionScale.vue
@@ -0,0 +1,14 @@
+<template>
+  <Transition
+    enter-active-class="transition-scale duration-300"
+    leave-active-class="transition-scale duration-300"
+    enter-from-class="scale-0"
+    leave-to-class="scale-0"
+  >
+    <slot />
+  </Transition>
+</template>
+
+<style>
+@import url(../assets/css/main.css);
+</style>
