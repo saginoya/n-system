939e4c0ac59fc6a4746ac81f12ce09ace92ac26f
saginoya <saginoya@create-nippo.co.jp>
2024-08-02 16:48:39 +0900

    Add NSwitchBookmark

--- src/components/NExhibitors.ce.vue
+++ src/components/NExhibitors.ce.vue
@@ -2,17 +2,25 @@
 import { ref } from 'vue'
 import NSwitch from '@/components/NSwitch.vue'
 import NTabs from '@/components/NTabs.vue'
+import NSwitcBookmark from '@/components/NSwitcBookmark.vue'
+import NTooltipBookmark from '@/components/NTooltipBookmark.vue'
 
 const nexpo = ref<boolean>(true)
 const gwpe = ref<boolean>(true)
 
 const sort = ref<'aiu' | 'koma'>('aiu')
 const sort2 = ref('aaa')
+
+const bookmark = ref<boolean>(false)
 </script>
 
 <template>
   <NSwitch id="nexpo" v-model="nexpo" label="NEW環境点" color="exhibition-a"></NSwitch>
   <NSwitch id="gwpe" v-model="gwpe" label="地球温暖化防止展" color="exhibition-b"></NSwitch>
+
+  <p>{{ nexpo }}</p>
+  <p>{{ gwpe }}</p>
+
   <NTabs
     v-model="sort"
     name="sort"
@@ -22,10 +30,12 @@ const sort2 = ref('aaa')
 
   <NTabs v-model="sort2" name="sort2" :values="['aaa', 'bbb', 'ccc']" color="info"></NTabs>
 
-  <p>{{ nexpo }}</p>
-  <p>{{ gwpe }}</p>
   <p>{{ sort }}</p>
   <p>{{ sort2 }}</p>
+
+  <NSwitcBookmark name="bookmark" v-model="bookmark" color="success"></NSwitcBookmark>
+  <NTooltipBookmark :active="bookmark"></NTooltipBookmark>
+  <p>{{ bookmark }}</p>
 </template>
 
 <style>
---
+++ src/components/NTooltipBookmark.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import IconBookmark from '@/components/icons/IconBookmark.vue'
+import { useColorStyle } from '@/utils/useColorStyle'
+import type { Color } from '@/types/color'
+
+const props = withDefaults(
+  defineProps<{
+    active: boolean
+    color?: Color
+  }>(),
+  {
+    color: 'primary'
+  }
+)
+const { textColor } = useColorStyle()
+
+const avtiveClass = [textColor(props.color)]
+const defaultClass = ['text-gray-200']
+
+const classState = computed(() => {
+  return props.active ? avtiveClass : defaultClass
+})
+</script>
+
+<template>
+  <button
+    type="button"
+    class="inline-flex size-8 items-center justify-center rounded-full bg-slate-50"
+    :class="classState"
+  >
+    <IconBookmark class="text-xl" />
+  </button>
+</template>
