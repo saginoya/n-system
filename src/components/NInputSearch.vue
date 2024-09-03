8b8c42c91465b2d2b8eef6083f50e7ae5f289f9b
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-09-03 15:50:42 +0900

    Add NInputSearch

---
+++ src/components/NInputSearch.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import IconSearch from '@/components/icons/IconSearch.vue'
+
+const model = defineModel<string>()
+</script>
+
+<template>
+  <label class="inline-flex items-center rounded-full border-2 px-3">
+    <IconSearch class="text-xl" />
+    <input type="search" class="border-0" v-model="model" />
+  </label>
+</template>
