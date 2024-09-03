f4e7a40c686aeef5c45c7a67863883e6eefd13ea
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-09-03 15:48:36 +0900

    Update NBtnClose

--- src/components/NBtnClose.vue
+++ src/components/NBtnClose.vue
@@ -3,9 +3,10 @@ import IconClose from '@/components/icons/IconClose.vue'
 </script>
 
 <template>
-  <div
-    class="flex size-8 items-center justify-center rounded-full bg-slate-800 p-1 text-2xl text-white hover:opacity-80"
+  <button
+    type="button"
+    class="flex size-8 items-center justify-center rounded-full bg-slate-800 p-1 text-2xl text-white"
   >
     <IconClose />
-  </div>
+  </button>
 </template>
