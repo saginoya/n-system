1abb03ffe690ed32f2abbfcecf084330736564e2
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-09-06 16:25:23 +0900

    Add NExhibitorListHeading & useExhibitorListHeading

---
+++ src/components/NExhibitorListHeading.vue
@@ -0,0 +1,7 @@
+<template>
+  <li class="py-2 text-center">
+    <span class="rounded-full bg-gray-100 px-4 text-sm">
+      <slot />
+    </span>
+  </li>
+</template>
---
+++ src/composables/useExhibitorListHeading.ts
@@ -0,0 +1,59 @@
+import { computed } from 'vue'
+import { useText } from '@/utils/useText'
+import type { Ref } from 'vue'
+import type { Lang } from '@/types/lang'
+import type { SortType } from '@/types/exhibitorList'
+import type { Exhibitor } from '@/types/exhibitorList'
+
+export const useExhibitorListHeading = (
+  exhibitorList: Ref<Exhibitor[]>,
+  sort: Ref<SortType>,
+  lang: Lang,
+  filterFunc: (value: Exhibitor) => boolean
+) => {
+  const { isSingleByteChara } = useText()
+
+  // 見出しの一覧
+  const headings = computed<string[]>(() => {
+    return exhibitorList.value.map((exhibitor) => {
+      if (filterFunc(exhibitor)) {
+        return showHeading(exhibitor.genre || '', exhibitor.order)
+      } else {
+        return ''
+      }
+    })
+  })
+
+  // 前の見出しと比較して一致するば値を返す
+  let currentHeading: string = ''
+  const showHeading = (genre: string, order: string): string => {
+    const value: string = generateHeading(genre, order)
+    if (currentHeading === value) {
+      return ''
+    } else {
+      currentHeading = value
+      return value
+    }
+  }
+
+  // 値から見出しも文字を生成
+  const generateHeading = (genre: string, order: string) => {
+    if (sort.value === 'koma') {
+      return genre
+    } else if (isOverseas(order.slice(0, 1))) {
+      return '海外'
+    } else {
+      return order.slice(0, 1)
+    }
+  }
+
+  // 海外の出展社かの判定（日本語限定）
+  const isOverseas = (order: string): boolean | undefined => {
+    if (lang !== 'ja') return undefined
+    return isSingleByteChara(order)
+  }
+
+  return {
+    headings
+  }
+}
