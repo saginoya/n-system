db867a45a504d603514322892e174bde7c9767a4
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-10-03 14:47:35 +0900

    Update useExhibitorListSort

--- src/composables/useExhibitorListSort.ts
+++ src/composables/useExhibitorListSort.ts
@@ -1,4 +1,5 @@
-import { ref, watchEffect } from 'vue'
+import { ref, computed, watchEffect } from 'vue'
+import { useSort } from '@/utils/useSort'
 import type { Ref } from 'vue'
 import type { Exhibitor, SortType } from '@/types/exhibitorList'
 
@@ -6,19 +7,23 @@ export const useExhibitorListSort = (exhibitorList: Ref<Exhibitor[]>) => {
   // ソートの条件
   const stateSort = ref<SortType>('name')
 
-  // ソートの条件と出展社のオブジェクトの相対関係
+  // ソートの条件からソートのキー名を返す
+  const stateSortKey = computed(() => {
+    return sortKeys[stateSort.value]
+  })
+  // ソートの条件とオブジェクトのキーの相関
   const sortKeys: Record<SortType, keyof Exhibitor> = {
     name: 'order',
     koma: 'koma'
   }
 
+  // ソートの関数読み込み
+  const { sortObjects } = useSort()
+
   // リストをソートする関数
   const sortExhibitorList = () => {
-    if (!stateSort.value) return
-    const key = sortKeys[stateSort.value]
-    exhibitorList.value.sort((a, b) => {
-      return (a[key] ?? 0) > (b[key] ?? 0) ? 1 : -1
-    })
+    if (!stateSort.value || exhibitorList.value.length < 2) return
+    exhibitorList.value = sortObjects<Exhibitor>(exhibitorList.value, stateSortKey.value)
   }
 
   // ソートの条件が変更されたら配列をソートする
---
+++ src/utils/useSort.ts
@@ -0,0 +1,43 @@
+export const useSort = () => {
+  // オブジェクトの配列に対するソート
+  // JSの組み込みオブジェクトsort()を利用しているため、アルゴリズムは実行エンジンごとに異なる。ブラウザごとに最適化されているため基本的にはこれを利用する
+  const sortObjects = <T>(arr: T[], key: keyof T): T[] => {
+    return arr.sort((a, b) => {
+      return (a[key] ?? '') > (b[key] ?? '') ? 1 : -1
+    })
+  }
+
+  // オブジェクトの配列にたいするマージソート
+  const mergeSortObjects = <T>(arr: T[], key: keyof T): T[] => {
+    // 配列の数が1以下の場合は早期リターン
+    if (arr.length <= 1) {
+      return arr
+    }
+
+    // ロジック
+    const merge = (left: T[], right: T[]): T[] => {
+      const results: T[] = []
+
+      while (left.length && right.length) {
+        if ((left[0][key] ?? '') < (right[0][key] ?? '')) {
+          results.push(left.shift()!)
+        } else {
+          results.push(right.shift()!)
+        }
+      }
+
+      return [...results, ...left, ...right]
+    }
+
+    const center = Math.floor(arr.length / 2)
+    const left = arr.slice(0, center)
+    const right = arr.slice(center)
+
+    return merge(mergeSortObjects(left, key), mergeSortObjects(right, key))
+  }
+
+  return {
+    sortObjects,
+    mergeSortObjects
+  }
+}
