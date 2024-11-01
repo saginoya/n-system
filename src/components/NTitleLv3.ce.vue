906453cf392efa09507ffb15a42373bbd1020c65
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-11-01 15:51:48 +0900

    Update NTitleLv3

--- src/components/NTitleLv3.ce.vue
+++ src/components/NTitleLv3.ce.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  note?: string
+}>()
+</script>
+
 <template>
-  <h3>
-    <slot />
+  <h3 class="flex flex-wrap items-center gap-2">
+    <span>
+      <slot />
+    </span>
+    <span v-if="note" class="text-base font-normal">
+      {{ note }}
+    </span>
   </h3>
 </template>
 
