32afc5cb7fcf833e8cf59b7482d40bf9f0af59a0
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-11-01 15:52:25 +0900

    Add NAnnotation

---
+++ src/components/NAnnotation.ce.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex gap-1 bg-slate-50 p-2 opacity-90">
+    <div>※</div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+@import url(../assets/css/main.css);
+</style>
