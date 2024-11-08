de8db322089c28dbfe3417d8c924ef0637908464
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-11-08 14:56:09 +0900

    Update NCard

--- src/components/NCard.ce.vue
+++ src/components/NCard.ce.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-defineProps<{
-  heightFull?: boolean
-}>()
+import { useVariantStyle } from '@/utils/useVariantStyle'
+import type { Variant } from '@/types/variant'
+import type { Color } from '@/types/color'
+
+withDefaults(
+  defineProps<{
+    heightFull?: boolean
+    variant?: Variant
+    color?: Color
+  }>(),
+  {
+    color: 'white'
+  }
+)
+
+const { getCardStyles } = useVariantStyle()
 </script>
 
 <template>
   <div
-    class="flex flex-col gap-4 rounded bg-white px-6 py-4"
-    :class="{ 'h-full': heightFull === true }"
+    class="flex flex-col gap-4 rounded border-2 px-6 py-4"
+    :class="[{ 'h-full': heightFull === true }, ...getCardStyles(color, variant)]"
   >
     <slot />
   </div>
--- src/utils/useVariantStyle.ts
+++ src/utils/useVariantStyle.ts
@@ -23,7 +23,28 @@ export const useVariantStyle = () => {
     }
   }
 
+  const getCardStyles = (color: Color, variant?: Variant): string[] => {
+    const bg: string = bgColor(color)
+    const border: string = borderColor(color)
+
+    const textWhite: string = textColor('white')
+    const bgWhite: string = bgColor('white')
+    const borderWhite: string = borderColor('white')
+
+    switch (variant) {
+      case 'flat':
+        return [textWhite, bg, border]
+      case 'outlined':
+        return [bgWhite, border]
+      case 'text':
+        return [bgWhite, borderWhite]
+      default:
+        return [bgWhite, borderWhite]
+    }
+  }
+
   return {
-    getStyles
+    getStyles,
+    getCardStyles
   }
 }
