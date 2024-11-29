847ad74731b6979b61a25d761cfc8b5084e7853c
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-11-29 15:00:10 +0900

    Update useFormFields

--- src/composables/useFormFields.ts
+++ src/composables/useFormFields.ts
@@ -6,22 +6,51 @@ import type { FormFields } from '@/types/formField'
 export const useFormFields = (storageKey: string, initFormFields: FormFields) => {
   const formFields = ref<FormFields>(initFormFields)
 
+  // ローカルストレージに保存するためのデータ形式
+  type StoredData = { [key: string]: string }
+  const storedData = ref<StoredData>({})
+
+  const clearStoredData = () => {
+    storedData.value = {}
+  }
+
+  const setFormFieldsToStoredData = () => {
+    clearStoredData()
+    for (const key in formFields.value) {
+      storedData.value[key] = formFields.value[key].value || ''
+    }
+  }
+
+  const setStoredDataToFormFields = () => {
+    if (storedData.value !== undefined) {
+      for (const key in storedData.value) {
+        formFields.value[key].value = storedData.value[key]
+      }
+    }
+  }
+
+  const setLocalStorageToStoredData = () => {
+    const localStorage = get()
+    if (localStorage) {
+      storedData.value = JSON.parse(localStorage)
+    }
+  }
+
   // ローカルストレージ操作関数の読み込み
   const { set, get } = useLocalStorage(storageKey)
 
   // ローカルストレージにformFieldsを保存する関数
   const saveFormFields = () => {
-    const formData = JSON.stringify(formFields.value)
+    setFormFieldsToStoredData()
+    const formData = JSON.stringify(storedData.value)
     set(formData)
     alert('フォームの内容が保存されました')
   }
 
   // ローカルストレージからformFieldsを読み込む関数
   const loadFormFields = () => {
-    const storedData = get()
-    if (storedData) {
-      formFields.value = JSON.parse(storedData)
-    }
+    setLocalStorageToStoredData()
+    setStoredDataToFormFields()
   }
 
   // フォームフィールドの値を更新する関数
