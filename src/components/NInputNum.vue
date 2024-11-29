b0b9a8583ce7c8f38641a2ba997addaca2f14c16
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-11-29 14:22:30 +0900

    Update NInputNum
    ValueをつねにString型として扱うように改良

--- src/components/NInputNum.vue
+++ src/components/NInputNum.vue
@@ -4,6 +4,12 @@ import type { FormFieldNumber } from '@/types/formField'
 defineProps<FormFieldNumber>()
 
 const model = defineModel<string>()
+
+// 入力値をString型として処理する関数
+const handleInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  model.value = target.value
+}
 </script>
 
 <template>
@@ -16,7 +22,8 @@ const model = defineModel<string>()
     :readonly
     :step
     inputmode="numeric"
-    v-model="model"
+    :value="model"
+    @input="handleInput"
     class="w-full max-w-xs rounded border-2 border-gray-300 px-2 py-1 text-lg"
   />
 </template>
