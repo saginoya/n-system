57df6ebfaf721e9c44438a53bd7f1e9f5f436bdd
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-12-02 11:31:46 +0900

    Add NOverlay

--- src/components/NModal.ce.vue
+++ src/components/NModal.ce.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import NBtn from '@/components/NBtn.ce.vue'
 import NCard from '@/components/NCard.ce.vue'
-import NTransitionFade from '@/components/NTransitionFade.vue'
+import NOverlay from '@/components/NOverlay.vue'
 import NTransitionScale from '@/components/NTransitionScale.vue'
 import NBtnClose from '@/components/NBtnClose.vue'
 import { useModal } from '@/composables/useModal'
@@ -24,25 +24,19 @@ defineExpose({ show })
 
 <template>
   <NBtn v-if="btnTitle" :color="btnColor" @click="show()">{{ btnTitle }}</NBtn>
-  <NTransitionFade>
-    <div
-      v-show="visible"
-      class="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-black/70 p-6"
-      @click="dismiss()"
-    >
-      <NTransitionScale>
-        <NCard ref="card" v-if="visible" @click.stop class="relative max-h-full" role="dialog">
-          <NBtnClose class="absolute -right-4 -top-4" @click="dismiss()"></NBtnClose>
-          <div class="overflow-y-auto overscroll-contain">
-            <slot />
-            <p class="text-center">
-              <NBtn color="gray" variant="text" @click="dismiss()">Close</NBtn>
-            </p>
-          </div>
-        </NCard>
-      </NTransitionScale>
-    </div>
-  </NTransitionFade>
+  <NOverlay v-show="visible" class="z-50" @click="dismiss()">
+    <NTransitionScale>
+      <NCard ref="card" v-if="visible" @click.stop class="relative max-h-full" role="dialog">
+        <NBtnClose class="absolute -right-4 -top-4" @click="dismiss()"></NBtnClose>
+        <div class="overflow-y-auto overscroll-contain">
+          <slot />
+          <p class="text-center">
+            <NBtn color="gray" variant="text" @click="dismiss()">Close</NBtn>
+          </p>
+        </div>
+      </NCard>
+    </NTransitionScale>
+  </NOverlay>
 </template>
 
 <style>
---
+++ src/components/NOverlay.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import NTransitionFade from '@/components/NTransitionFade.vue'
+</script>
+
+<template>
+  <NTransitionFade>
+    <div
+      class="fixed left-0 top-0 flex h-screen w-full items-center justify-center bg-black/70 p-6"
+    >
+      <slot />
+    </div>
+  </NTransitionFade>
+</template>
