ab6bff0378563459d3b9c2987d0bbb7eecdb966f
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-12-11 16:23:49 +0900

    Update components/NInputCheckbox

--- src/components/NInputCheckbox.vue
+++ src/components/NInputCheckbox.vue
@@ -3,14 +3,20 @@ defineProps<{
   name: string
   value: string
   checked?: boolean
+  disabled?: boolean
 }>()
 
 const model = defineModel<boolean>()
+
+const defaultStyle = 'cursor-pointer rounded-full bg-gray-100 px-4 py-1 text-lg'
+const hoverStyle = 'hover:bg-gray-200 focus:bg-gray-200'
+const checkedStyle = 'has-[:checked]:bg-success has-[:checked]:text-white'
+const disabledStyle = 'has-[:disabled]:cursor-default  has-[:disabled]:text-gray-300'
 </script>
 
 <template>
-  <label class="rounded px-2 py-1 text-lg hover:bg-slate-100 focus:bg-slate-100">
-    <input type="checkbox" :name :value :checked v-model="model" />
+  <label :class="[defaultStyle, hoverStyle, checkedStyle, disabledStyle]">
+    <input type="checkbox" :name :value :checked :disabled v-model="model" class="sr-only" />
     <slot />
   </label>
 </template>
