e3050abce2c08c34f3a3051143d577ec5219994d
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-12-18 16:39:40 +0900

    Update NCharacterCount

--- src/components/atoms/NCharacterCount.vue
+++ src/components/atoms/NCharacterCount.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-defineProps<{
-  value?: string
+export type CharacterCountProps = {
+  value?: string | string[]
   maxlength?: number
   minlength?: number
-}>()
+}
+
+defineProps<CharacterCountProps>()
 </script>
 
 <template>
   <div>
+    <span v-show="minlength">{{ minlength }} &lE; </span>
     <span>{{ value?.length || 0 }}</span>
     <span v-show="maxlength"> / {{ maxlength }}</span>
-    <span v-show="minlength"> ({{ minlength }}文字以上)</span>
   </div>
 </template>
