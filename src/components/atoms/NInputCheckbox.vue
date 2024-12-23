b3c8bbb463c917e797ae474fdcdff014c84d0153
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-12-23 16:38:27 +0900

    Update NInputCheckbox

--- src/components/atoms/NInputCheckbox.vue
+++ src/components/atoms/NInputCheckbox.vue
@@ -1,14 +1,15 @@
-<script setup lang="ts" generic="T">
-export type NInputCheckboxProps<T> = {
+<script setup lang="ts">
+export type NInputCheckboxProps = {
   name: string
-  value: T
+  value: string
+  label?: string
   checked?: boolean
   disabled?: boolean
 }
 
-defineProps<NInputCheckboxProps<T>>()
+defineProps<NInputCheckboxProps>()
 
-const model = defineModel<boolean>()
+const model = defineModel<string[]>()
 
 const defaultStyle = 'cursor-pointer rounded-full bg-gray-100 px-4 py-1 text-lg'
 const hoverStyle = 'hover:bg-gray-200 focus:bg-gray-200'
@@ -19,6 +20,8 @@ const disabledStyle = 'has-[:disabled]:cursor-default  has-[:disabled]:text-gray
 <template>
   <label :class="[defaultStyle, hoverStyle, checkedStyle, disabledStyle]">
     <input type="checkbox" :name :value :checked :disabled v-model="model" class="sr-only" />
-    <slot />
+    <slot>
+      {{ label || value }}
+    </slot>
   </label>
 </template>
