6cdfba874d9d00750176d378eb8ac97b7fd0d7cf
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-12-25 15:15:50 +0900

    Update vertion

--- vite.config.ts
+++ vite.config.ts
@@ -10,29 +10,29 @@ export default defineConfig({
       template: {
         compilerOptions: {
           // ダッシュを含むすべてのタグをカスタム要素として扱う
-          isCustomElement: (tag) => tag.includes('-')
-        }
-      }
-    })
+          isCustomElement: (tag) => tag.includes('-'),
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   build: {
     rollupOptions: {
       output: {
         entryFileNames: `assets/n-system.js`,
-        assetFileNames: `assets/n-system.[ext]`
-      }
-    }
+        assetFileNames: `assets/n-system.[ext]`,
+      },
+    },
   },
   server: {
     watch: {
       usePolling: true,
       interval: 300,
-      ignored: ['**/node_modules/**', '**/dist/**']
-    }
-  }
+      ignored: ['**/node_modules/**', '**/dist/**'],
+    },
+  },
 })
