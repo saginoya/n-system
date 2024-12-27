c29901932621e90a3a169ef95b4bd8e39cf49f2d
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2024-12-27 15:22:34 +0900

    Update NListNav

---
+++ src/components/atoms/NLinkBlock.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import NLinkText from '@/components/atoms/NLinkText.vue'
+import { useURL } from '@/utils/useURL'
+import type { LinkType } from '@/types/link'
+
+defineProps<{
+  href: string
+  type: LinkType
+}>()
+
+const { isSamePage } = useURL()
+</script>
+
+<template>
+  <NLinkText
+    class="block rounded-full px-3 py-1 hover:bg-blue-200/30 focus:bg-blue-200/30"
+    :class="{
+      'bg-slate-900/40': isSamePage(href),
+      'contrast-75': isSamePage(href),
+    }"
+    :href
+    :type
+  >
+    <slot />
+  </NLinkText>
+</template>
--- src/components/molecules/NListNav.vue
+++ src/components/molecules/NListNav.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NLink from '@/components/atoms/NLink.ce.vue'
+import NLinkBlock from '@/components/atoms/NLinkBlock.vue'
 import type { Link } from '@/types/link'
 
 defineProps<{
@@ -10,13 +10,9 @@ defineProps<{
 <template>
   <ul class="flex flex-col gap-1">
     <li v-for="(link, index) in links" :key="index">
-      <n-link :href="link.path" :type="link.type" class="font-bold text-white">
+      <NLinkBlock :href="link.path" :type="link.type" class="font-bold text-white">
         {{ link.name }}
-      </n-link>
+      </NLinkBlock>
     </li>
   </ul>
 </template>
-
-<style>
-@import url(@/assets/css/main.css);
-</style>
