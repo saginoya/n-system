5450fea3618c2dc68a12eb105e1907b8e08b2d0e
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-01-17 13:54:57 +0900

    Update atoms/NListitemApp

--- src/components/atoms/NListItemApp.ce.vue
+++ src/components/atoms/NListItemApp.ce.vue
@@ -1,65 +1,48 @@
 <script setup lang="ts">
+import NChip from '@/components/atoms/NChip.ce.vue'
+
 defineProps<{
   id: string
   data: string
   dataType: string
-  dataSize: number
-  period: string
-  destination: string
-  email: string
-  required?: boolean
+  dataSize?: string
   showHeader?: boolean
 }>()
 
-const headers = ['項目', 'データ', '提出書類内容', '提出期限', '提出先', '対象']
+const headers = ['様式', '書類内容', 'ダウンロード']
 </script>
 
 <template>
   <li
     v-if="showHeader"
-    class="grid grid-cols-12 items-center divide-x border-y bg-slate-100 p-1 text-center font-bold"
+    class="hidden list-none items-center divide-x border-y bg-slate-100 p-1 text-center font-bold md:grid md:grid-cols-5"
   >
-    <span class="col-span-1">
+    <div class="col-span-1">
       {{ headers[0] }}
-    </span>
-    <span class="col-span-1">
+    </div>
+    <div class="col-span-3">
       {{ headers[1] }}
-    </span>
-    <span class="col-span-4">
+    </div>
+    <div class="col-span-1">
       {{ headers[2] }}
-    </span>
-    <span class="col-span-2">
-      {{ headers[3] }}
-    </span>
-    <span class="col-span-2">
-      {{ headers[4] }}
-    </span>
-    <span class="col-span-2">
-      {{ headers[5] }}
-    </span>
+    </div>
   </li>
-  <li class="grid grid-cols-12 items-center border-y p-1">
-    <span class="col-span-1 text-center">
+  <li class="grid list-none grid-cols-1 items-center border-y p-1 hover:bg-blue-50 md:grid-cols-5">
+    <div class="col-span-1 bg-slate-100 font-bold md:bg-inherit md:text-center">
       {{ id }}
-    </span>
-    <span class="col-span-1 text-center">
-      <a :href="data" download
-        >{{ dataType }}<br />
-        ({{ dataSize }}KB)</a
-      >
-    </span>
-    <span class="col-span-4">
+    </div>
+    <div class="col-span-1 md:col-span-3">
       <slot />
-    </span>
-    <span class="col-span-2 text-center">{{ period }}</span>
-    <span class="col-span-2 text-center">
-      <p>{{ destination }}</p>
-      <p>{{ email }}</p>
-    </span>
-    <span class="col-span-2 text-center">
-      <span v-if="required" class="text-red-500">全出展社必須</span>
-      <span v-else>該当出展社のみ</span>
-    </span>
+    </div>
+    <div class="col-span-1 text-center">
+      <p>
+        <NChip color="success" :href="data" type="download">ダウンロード</NChip>
+      </p>
+      <p class="flex justify-center gap-1 text-sm">
+        <span>{{ dataType }}</span>
+        <span v-if="dataSize">{{ dataSize }}</span>
+      </p>
+    </div>
   </li>
 </template>
 
