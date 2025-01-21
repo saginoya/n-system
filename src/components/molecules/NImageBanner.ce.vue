ec917468c390bbecad7640a1d41bda49d0d2b11a
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-01-21 16:06:09 +0900

    Fix molecules/NImageBanner

--- src/components/molecules/NImageBanner.ce.vue
+++ src/components/molecules/NImageBanner.ce.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
+import NImage from '@/components/molecules/NImage.ce.vue'
 import { useLink } from '@/utils/useLink'
 import type { LinkType } from '@/types/link'
+import type { Image } from '@/types/image'
 
-defineProps<{
-  src: string
-  alt: string
-  href?: string
-  type?: LinkType
-}>()
+const props = defineProps<
+  Image & {
+    href?: string
+    type?: LinkType
+  }
+>()
 
 const { linkTarget, linkIcon } = useLink()
+
+const icon = props.type ? linkIcon(props.type) : undefined
 </script>
 
 <template>
@@ -17,13 +21,13 @@ const { linkTarget, linkIcon } = useLink()
     :is="href ? 'a' : 'span'"
     :href="href ? href : null"
     :target="href ? (type ? linkTarget(type) : '_self') : null"
-    class="relative h-auto w-full"
+    class="relative inline-block w-fit max-w-full"
     :class="href ? 'hover:opacity-60' : undefined"
   >
-    <img :src :alt />
-    <div v-if="type" class="absolute left-0 top-0 flex size-full items-center justify-end">
+    <NImage :src :alt :width :height></NImage>
+    <div v-if="icon" class="absolute left-0 top-0 flex size-full items-center justify-end">
       <span class="rounded-l-full bg-info py-2 pl-1.5 text-white">
-        <component :is="linkIcon(type)" class="size-6" />
+        <component :is="icon" class="size-6" />
       </span>
     </div>
   </component>
