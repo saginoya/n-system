99d9200859fd8b85888163835b93443b7b75fa6d
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-01-29 15:55:26 +0900

    Update NContainerFlex

--- src/components/atoms/NContainerFlex.ce.vue
+++ src/components/atoms/NContainerFlex.ce.vue
@@ -1,9 +1,26 @@
+<script setup lang="ts">
+import { justifyOptions, justifyMdOptions } from '@/utils/styleHelpers/useLayoutStyle'
+import type { Justify } from '@/utils/styleHelpers/useLayoutStyle'
+
+const props = defineProps<{
+  justify?: Justify
+  justifyMd?: Justify
+}>()
+
+const justifyStyles = (): (string | null)[] => {
+  return [
+    props.justify ? justifyOptions[props.justify] : null,
+    props.justifyMd ? justifyMdOptions[props.justifyMd] : null,
+  ]
+}
+</script>
+
 <template>
-  <div class="flex flex-wrap items-center gap-2">
+  <div class="flex flex-wrap items-center gap-2" :class="justifyStyles()">
     <slot />
   </div>
 </template>
 
 <style>
-@import url(@/assets/css/main.css);
+@import url('@/assets/css/main.css');
 </style>
---
+++ src/utils/styleHelpers/useLayoutStyle.ts
@@ -0,0 +1,25 @@
+export const justifyOptions = {
+  start: 'justify-start',
+  end: 'justify-end',
+  center: 'justify-center',
+  between: 'justify-between',
+  around: 'justify-around',
+  evenly: 'justify-evenly',
+  stretch: 'justify-stretch',
+  baseline: 'justify-baseline',
+  normal: 'justify-normal',
+}
+
+export type Justify = keyof typeof justifyOptions
+
+export const justifyMdOptions: Record<Justify, string> = {
+  start: 'md:justify-start',
+  end: 'md:justify-end',
+  center: 'md:justify-center',
+  between: 'md:justify-between',
+  around: 'md:justify-around',
+  evenly: 'md:justify-evenly',
+  stretch: 'md:justify-stretch',
+  baseline: 'md:justify-baseline',
+  normal: 'md:justify-normal',
+}
