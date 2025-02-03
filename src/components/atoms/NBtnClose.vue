472209aff406a458e2b1a85bd6553569232da089
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-02-03 15:20:41 +0900

    Update atoms/NBtnClose

--- src/components/atoms/NBtnClose.vue
+++ src/components/atoms/NBtnClose.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 import IconClose from '@/components/icons/IconClose.vue'
+
+withDefaults(
+  defineProps<{
+    size?: Size
+  }>(),
+  {
+    size: 'md',
+  },
+)
+
+const sizeOptions = {
+  sm: 'size-6 text-base p-0.5',
+  md: 'size-8 text-2xl p-1',
+  lg: 'size-12 text-4xl p-2',
+}
+
+type Size = keyof typeof sizeOptions
 </script>
 
 <template>
   <button
     type="button"
-    class="flex size-8 items-center justify-center rounded-full bg-slate-800 p-1 text-2xl text-white"
+    class="flex items-center justify-center rounded-full bg-slate-800 text-white"
+    :class="sizeOptions[size]"
   >
     <IconClose />
   </button>
