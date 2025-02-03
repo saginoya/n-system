923c533c6d1f73c737265bb084359239c85ab9c9
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-02-03 15:23:26 +0900

    Add atoms/NText

---
+++ src/components/atoms/NText.ce.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  size: SizeOptions
+}>()
+
+const sizeOptions = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  base: 'text-base',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+  '4xl': 'text-4xl',
+  '5xl': 'text-5xl',
+  '6xl': 'text-6xl',
+}
+type SizeOptions = keyof typeof sizeOptions
+</script>
+
+<template>
+  <span :class="sizeOptions[size]">
+    <slot />
+  </span>
+</template>
+
+<style>
+@import url('@/assets/css/main.css');
+</style>
