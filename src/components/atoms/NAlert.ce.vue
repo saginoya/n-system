75cbf2ee4a8402b5ac091ac0e5adc8fb64e16478
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-02-03 15:35:49 +0900

    Update NAlert

--- src/components/atoms/NAlert.ce.vue
+++ src/components/atoms/NAlert.ce.vue
@@ -18,8 +18,8 @@ const colorStyles = [textColor(props.type), borderColor(props.type)]
 </script>
 
 <template>
-  <div class="flex items-center gap-2 rounded border p-2" :class="colorStyles">
-    <div class="p-2 text-4xl text-current opacity-50">
+  <div class="flex flex-col items-center gap-2 rounded border p-2 md:flex-row" :class="colorStyles">
+    <div class="p-0 text-4xl text-current opacity-50 md:p-2">
       <component :is="type === 'info' ? IconInfo : IconWaring" />
     </div>
     <div class="text-current">
