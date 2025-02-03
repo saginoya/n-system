6ed14b0bb0f04d3b0c121d605e5f73b6ce4a20d5
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-02-03 15:22:22 +0900

    Update composables/useModal

--- src/composables/useModal.ts
+++ src/composables/useModal.ts
@@ -1,7 +1,7 @@
 import { ref, readonly } from 'vue'
 
-export const useModal = () => {
-  const visible = ref(false)
+export const useModal = (initValue?: boolean) => {
+  const visible = ref(initValue || false)
   const show = () => (visible.value = true)
   const dismiss = () => (visible.value = false)
 
