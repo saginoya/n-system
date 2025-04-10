188c47c4210316dc603e1a10284dc940af1b816b
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-04-10 11:11:15 +0900

    Update NTooltipInfo

--- src/components/molecules/NTooltipInfo.ce.vue
+++ src/components/molecules/NTooltipInfo.ce.vue
@@ -12,13 +12,13 @@ const text = lang.value === 'en' ? 'Information' : 'インフォメーション'
 const locationStyles = (): string[] => {
   switch (props.location) {
     case 'bottom':
-      return ['sm:top-10', 'sm:-translate-y-0']
+      return ['sm:bottom-1', 'sm:top-auto', 'sm:translate-y-full']
     case 'right':
-      return ['sm:left-32', 'sm:-translate-y-1/2']
+      return ['sm:-left-1', 'sm:-translate-x-full']
     case 'left':
-      return ['sm:right-32', 'sm:left-auto', 'sm:-translate-y-1/2']
+      return ['sm:-right-1', 'sm:left-auto', 'sm:translate-x-full']
     default:
-      return ['sm:bottom-10', 'sm:top-auto', 'sm:-translate-y-0']
+      return ['sm:top-1', 'sm:-translate-y-full']
   }
 }
 </script>
@@ -28,7 +28,7 @@ const locationStyles = (): string[] => {
     class="group relative inline-block cursor-pointer rounded p-1 align-middle hover:bg-slate-100 focus:bg-slate-100"
   >
     <IconInfo class="inline text-xl" />
-    <span class="text-xs font-bold tracking-tight">{{ text }}</span>
+    <span class="hidden text-xs font-bold tracking-tight md:inline-block">{{ text }}</span>
     <div
       class="invisible fixed left-0 top-1/2 z-50 m-2 -translate-y-1/2 rounded bg-slate-800 p-2 text-sm text-white opacity-0 transition-all delay-150 ease-in-out group-hover:visible group-hover:opacity-100 group-focus:visible group-focus:opacity-100 sm:absolute sm:m-0 sm:w-96"
       :class="locationStyles()"
