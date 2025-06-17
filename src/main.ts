cd19ae03b78b0f8c3f20431cc2afa372b4fe1266
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-06-17 16:10:29 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,14 @@ const components = import.meta.glob<{ default: DefineComponent }>(
 Object.entries(components).forEach(([path, component]) => {
   const fileName = path.split('/').pop() || ''
   const baseName = fileName.replace('.ce.vue', '')
+  // パスカルケースをケバブケースに変換
+  // *数字が連続するとその都度ハイフンが入るので注意
   const componentName = baseName
-    .replace(/([A-Z])/g, '-$1')
-    .replace(/(\d+)/g, '-$1')
-    .toLowerCase()
+    .replace(/([A-Z])/g, '-$1') // 大文字の前にハイフンを追加
+    .replace(/(\d+)/g, '-$1') // 数字の前にハイフンを追加
+    .toLowerCase() // 小文字に変換
     .replace(/^-|-$/g, '') // 先頭と末尾のハイフンを削除
+    .replace(/-+/g, '-') // 重複ハイフンを1つに
 
   if (componentName) {
     customElements.define(componentName, defineCustomElement(component.default))
