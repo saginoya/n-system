9f7225ac974a27b8ba7b486fd4b048349f3ef1a4
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-06-24 16:27:50 +0900

    Update MarkupText

--- src/components/parts/MarkupText.vue
+++ src/components/parts/MarkupText.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
+import DOMPurify from 'dompurify'
 import { marked } from 'marked'
-import sanitizeHtml from 'sanitize-html'
 import { computed } from 'vue'
 
 marked.setOptions({
@@ -11,25 +11,15 @@ const props = withDefaults(
   defineProps<{
     content: string
     isMarkdown?: boolean
-    allowedTags?: string[]
-    allowedAttributes?: Record<string, string[]>
   }>(),
   {
     isMarkdown: false,
-    allowedTags: () => ['p', 'br', 'strong', 'em', 'a', 'ul', 'ol', 'li'],
-    allowedAttributes: () => ({
-      a: ['href', 'target', 'rel'],
-    }),
   },
 )
 
 const sanitizedContent = computed(() => {
   const rawContent = props.isMarkdown ? (marked.parse(props.content) as string) : props.content
-
-  return sanitizeHtml(rawContent, {
-    allowedTags: props.allowedTags,
-    allowedAttributes: props.allowedAttributes,
-  })
+  return DOMPurify.sanitize(rawContent)
 })
 </script>
 
