878371f01602fa2834c4a8db9529e04b9d91a7b3
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-07-07 14:30:29 +0900

    Fix LayoutDefault

--- src/components/layouts/LayoutDefault.vue
+++ src/components/layouts/LayoutDefault.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-
 import GlobalHeader from '@/components/parts/GlobalHeader.vue'
 import HeaderTool from '@/components/parts/HeaderTool.vue'
 import OverlayBase from '@/components/parts/OverlayBase.vue'
@@ -8,27 +6,17 @@ import ToolBar from '@/components/parts/ToolBar.vue'
 import ToolFooter from '@/components/parts/ToolFooter.vue'
 import { useConfig } from '@/composables/useConfig'
 import { useDrawer } from '@/composables/useDrawer'
-import { useRouter } from '@/composables/useRouter'
-import type { Router } from '@/types'
-
-const { getByIds, isReady } = useRouter()
 
 const props = defineProps<{
   jsonPath: string
 }>()
 const { siteTitle, copyright, navigation } = useConfig(props.jsonPath)
 const { drawer, operateDrawer, dismissDrawer } = useDrawer()
-
-const headerLinks = ref<Router[]>([])
-const footerLinks = ref<Router[]>([])
-
-watch(isReady, () => {
-  headerLinks.value = getByIds(['sample', 'top'])
-  footerLinks.value = getByIds(['sample', 'bottom'])
-})
 </script>
 
 <template>
+  <OverlayBase v-show="drawer" class="z-30" @click="dismissDrawer()"></OverlayBase>
+  <HeaderTool :active="drawer" class="z-50" @click="operateDrawer()"></HeaderTool>
   <GlobalHeader
     v-if="siteTitle && navigation"
     :titleList="siteTitle"
@@ -36,8 +24,6 @@ watch(isReady, () => {
     class="fixed left-0 top-0 z-40 h-screen w-96 max-w-full -translate-x-96 transition duration-200"
     :class="{ 'translate-x-0': drawer }"
   ></GlobalHeader>
-  <OverlayBase v-show="drawer" class="z-30" @click="dismissDrawer()"></OverlayBase>
-  <HeaderTool :active="drawer" class="z-50" @click="operateDrawer()"></HeaderTool>
   <div class="flex min-h-screen w-full flex-col overflow-hidden bg-slate-200">
     <ToolBar :title="siteTitle ? siteTitle.join() : undefined" :navigation />
     <main class="flex flex-1 justify-center px-4 py-8">
