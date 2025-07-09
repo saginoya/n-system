483d82e6cd05602fd5b90653cc1ebc700b2c1471
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-07-09 14:25:54 +0900

    Update NChipStack

---
+++ src/components/n-elements/NChipStack.ce.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import NContainerFlex from '@/components/n-elements/NContainerFlex.ce.vue'
+const props = defineProps<{
+  items: string
+}>()
+
+const arr = props.items.split(',')
+</script>
+
+<template>
+  <NContainerFlex>
+    <span v-for="(item, index) in arr" :key="index" class="rounded bg-gray-200/50 px-2 py-0.5">
+      {{ item }}
+    </span>
+  </NContainerFlex>
+</template>
+
+<style>
+@import url(@/assets/css/main.css);
+</style>
