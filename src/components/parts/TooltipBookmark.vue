c44d11891e9790d0f7c34031afd17d70b235459e
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-09-10 11:25:06 +0900

    Update TooltipBookmark Component

--- src/components/parts/TooltipBookmark.vue
+++ src/components/parts/TooltipBookmark.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-import IconBookmark from '@/components/icons/IconBookmark.vue'
+import TooltipBase from '@/components//parts/TooltipBase.vue'
+import { useLang } from '@/composables/useLang'
 import { textColorMap } from '@/styles'
 import type { Color } from '@/types'
+import { iconMap } from '@/utils'
 
 const props = withDefaults(
   defineProps<{
@@ -12,23 +14,43 @@ const props = withDefaults(
   }>(),
   {
     color: 'primary',
+    lang: 'ja',
   },
 )
 
 const avtiveClass = [textColorMap[props.color]]
-const defaultClass = ['text-gray-200']
+const defaultClass = ['text-gray-300']
 
 const classState = computed(() => {
   return props.active ? avtiveClass : defaultClass
 })
+
+const IconBookmark = iconMap['bookmark']
+
+const { lang } = useLang()
+const textMap = {
+  ja: {
+    active: 'お気に入り解除',
+    inactive: 'お気に入り登録',
+  },
+  en: {
+    active: 'Remove from favorites',
+    inactive: 'Add to Favorites',
+  },
+}
 </script>
 
 <template>
-  <button
-    type="button"
-    class="inline-flex size-8 items-center justify-center rounded-full bg-slate-50"
-    :class="classState"
-  >
-    <IconBookmark class="text-xl" />
-  </button>
+  <TooltipBase>
+    <button
+      type="button"
+      class="inline-flex size-8 items-center justify-center rounded-full bg-slate-50"
+      :class="classState"
+    >
+      <IconBookmark
+        class="text-xl transition-transform duration-100 ease-in-out active:scale-125"
+      />
+    </button>
+    <template v-slot:text> {{ textMap[lang][active ? 'active' : 'inactive'] }} </template>
+  </TooltipBase>
 </template>
