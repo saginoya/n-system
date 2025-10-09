14d8e5ce2d18b8f5865e6728d1ecc9607f7d75a7
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-09 13:08:24 +0900

    Update NAnnotation

--- src/components/n-elements/NAnnotation.ce.vue
+++ src/components/n-elements/NAnnotation.ce.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-defineProps<{
-  mark?: string
-}>()
+import { cn } from '@/lib/cn'
+
+withDefaults(
+  defineProps<{
+    mark?: string
+    type?: 'default' | 'light'
+  }>(),
+  {
+    mark: '※',
+    type: 'default',
+  },
+)
+
+const typeClasses = {
+  default: 'gap-1 border-l-2 border-l-info bg-gray-50 p-2',
+  light: 'gap-0.5 text-xs italic text-gray-600 md:justify-end',
+}
 </script>
 
 <template>
-  <div class="flex gap-1 border-l-2 border-l-info bg-gray-50 p-2">
-    <div>{{ mark || '※' }}</div>
+  <div :class="cn('flex', typeClasses[type])">
+    <div>{{ mark }}</div>
     <div>
       <slot />
     </div>
