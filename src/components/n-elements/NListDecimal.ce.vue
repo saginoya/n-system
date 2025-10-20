bf12cabb395164aedc3f2e3a6defcdb23eee7371
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-20 14:52:05 +0900

    Update NListDecimal

--- src/components/n-elements/NListDecimal.ce.vue
+++ src/components/n-elements/NListDecimal.ce.vue
@@ -7,12 +7,12 @@ const itemList: string[] = props.items ? props.items.split(',') : []
 </script>
 
 <template>
-  <ul class="list-decimal pl-6 [&>li]:py-0.5">
+  <ol class="list-decimal pl-6 [&>li]:py-0.5">
     <li v-for="(item, index) in itemList" :key="index">
       {{ item }}
     </li>
     <slot />
-  </ul>
+  </ol>
 </template>
 
 <style>
