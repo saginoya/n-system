b21285dc0c4d074e2e0316eec396629ea30bacf1
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-23 16:19:01 +0900

    Update TooltipBase

--- src/components/parts/TooltipBase.vue
+++ src/components/parts/TooltipBase.vue
@@ -10,17 +10,10 @@ export type TooltipProps = {
 withDefaults(defineProps<TooltipProps>(), { location: 'top', color: 'light' })
 
 const locationStyleMap = {
-  top: ['sm:-top-1', 'sm:left-1/2', 'sm:right-auto', 'sm:-translate-y-full', 'sm:-translate-x-1/2'],
-  bottom: [
-    'sm:-bottom-1',
-    'sm:top-auto',
-    'sm:left-1/2',
-    'sm:right-auto',
-    'sm:translate-y-full',
-    'sm:-translate-x-1/2',
-  ],
-  right: ['sm:-right-1', 'sm:left-auto', 'sm:translate-x-full'],
-  left: ['sm:-left-1', 'sm:right-auto', 'sm:-translate-x-full'],
+  top: '-top-1 left-1/2 -translate-y-full -translate-x-1/2',
+  bottom: '-bottom-1 left-1/2 translate-y-full -translate-x-1/2',
+  right: '-right-1 top-1/2 translate-x-full -translate-y-1/2',
+  left: '-left-1 top-1/2 -translate-x-full -translate-y-1/2',
 }
 
 const colorStyleMap = {
@@ -35,7 +28,7 @@ const colorStyleMap = {
     <div
       :class="
         cn(
-          'rounded invisible fixed inset-x-0 top-1/2 z-50 m-2 -translate-y-1/2  p-2 text-sm opacity-0 transition-all delay-150 ease-in-out group-hover:visible group-hover:opacity-100 group-focus:visible group-focus:opacity-100 sm:absolute sm:m-0 sm:inline-block sm:whitespace-nowrap shadow-lg',
+          'rounded invisible z-50 p-2 text-sm opacity-0 transition-all delay-150 ease-in-out group-hover:visible group-hover:opacity-100 group-focus:visible group-focus:opacity-100 absolute inline-block whitespace-nowrap shadow-lg',
           locationStyleMap[location],
           colorStyleMap[color],
           className,
