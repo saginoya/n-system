a41e1a4276d9e18c7c9d65d8b537c6a0bba339ad
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-23 16:19:32 +0900

    Update NTooltipInfo

--- src/components/n-elements/NTooltipInfo.ce.vue
+++ src/components/n-elements/NTooltipInfo.ce.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import TooltipBase from '@/components//parts/TooltipBase.vue'
-import IconInfo from '@/components/icons/IconInfo.vue'
+import { iconMap, type Icon } from '@/utils'
 import { useLang } from '@/composables/useLang'
 
-defineProps<{
-  location?: 'top' | 'bottom' | 'right' | 'left'
-}>()
+withDefaults(
+  defineProps<{
+    location?: 'top' | 'bottom' | 'right' | 'left'
+    icon?: Icon
+  }>(),
+  {
+    location: 'top',
+    icon: 'info',
+  },
+)
 
 const { lang } = useLang()
 const label = lang.value === 'en' ? 'Information' : 'インフォメーション'
@@ -17,8 +24,8 @@ const label = lang.value === 'en' ? 'Information' : 'インフォメーション
     color="dark"
     class-name="cursor-pointer rounded p-1 align-middle hover:bg-slate-100 focus:bg-slate-100"
   >
-    <IconInfo class="inline text-xl" />
-    <span class="hidden text-xs font-bold tracking-tight md:inline-block">
+    <component :is="iconMap[icon]" class="inline bg-slate-800 rounded-full text-white" />
+    <span class="hidden text-xs font-bold tracking-tight md:inline-block ml-1">
       {{ label }}
     </span>
     <template v-slot:text>
