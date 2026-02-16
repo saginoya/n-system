6bc5c3f99a07a9b0f5c963c41dc36cd74827236e
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2026-02-16 14:07:02 +0900

    feat: Add NSystemInfo component

---
+++ src/components/entries/media/NSystemInfo.ce.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import packageJson from '@/../package.json'
+
+const version: string = packageJson.version
+</script>
+
+<template>
+  <article>
+    <h1>N-System Info</h1>
+    <ul>
+      <li>Version: {{ version }}</li>
+    </ul>
+  </article>
+</template>
+
+<style>
+@import url(@/assets/css/main.css);
+</style>
