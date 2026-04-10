957eef2f822bccf47a5abb0d8bd9ce7c5b60990a
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2026-04-10 14:16:14 +0900

    feat: フォーマットの実行

--- src/components/entries/interaction/NTooltipCampaign.ce.vue
+++ src/components/entries/interaction/NTooltipCampaign.ce.vue
@@ -8,18 +8,23 @@ import { iconMap } from '@/utils'
   <Menu as="div" class="relative inline-block">
     <div>
       <MenuButton
-        class="inline-flex justify-center items-center rounded-full p-2 text-2xl cursor-pointer hover:bg-black/10 hover:-rotate-15 duration-300">
+        class="inline-flex justify-center items-center rounded-full p-2 text-2xl cursor-pointer hover:bg-black/10 hover:-rotate-15 duration-300"
+      >
         <component :is="iconMap['campaign']" />
       </MenuButton>
     </div>
 
     <transition
-      enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0"
-      enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in"
-      leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0"
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
     >
       <MenuItems
-        class="absolute right-0 mt-2 rounded min-w-2xs max-w-md bg-white shadow-lg overflow-hidden p-4 z-10 border border-gray-200">
+        class="absolute right-0 mt-2 rounded min-w-2xs max-w-md bg-white shadow-lg overflow-hidden p-4 z-10 border border-gray-200"
+      >
         <slot />
       </MenuItems>
     </transition>
